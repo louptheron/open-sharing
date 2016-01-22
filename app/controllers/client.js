801d3a63cc102574c14c8b0d9fbdaded973c9b2b
louptheron <loup-rodney@hotmail.fr>
2016-01-22 17:07:30 +0100

    client send changes when filechange

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -1,11 +1,26 @@
 'use strict';
 
 import * as net from 'net';
+import * as fs from 'fs';
+import * as utils from './utils'
 
 var client = new net.Socket();
 client.connect(1337, '127.0.0.1', function() {
     console.log('Connected');
     client.write('Hello, server! Love, Client.');
+
+    fs.watch(utils.getUserDir(), function(event, filename) {
+        console.log(`event is: ${event}`);
+        if (filename) {
+            console.log(`filename provided: ${filename}`);
+        } else {
+            console.log('filename not provided');
+        }
+        fs.readFile(utils.getUserDir() + '/' + filename, function(err, data){
+            //broadcast(data, socket);
+            client.write(data, 'binary');
+        });
+    });
 });
 
 client.on('data', function(data) {
