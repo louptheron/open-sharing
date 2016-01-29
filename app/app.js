1d2312465c2ff96ed67b4958625d6a1a19b035ac
louptheron <loup-rodney@hotmail.fr>
2016-01-29 13:44:17 +0100

    fix bug when first launch : check if the DB exist

--- app/app.js
+++ app/app.js
@@ -29,44 +29,28 @@ function addUser(){
     });
 }
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files like it is node.js! Welcome to Electron world :)
-console.log('The author of this app is:', appDir.read('package.json', 'json').author);
-
 document.addEventListener('DOMContentLoaded', function () {
-
     ipcRenderer.send('isUsernameDB');
     ipcRenderer.on('isUsernameDB', function(event, msg){
         if(msg > 1){
-            return;
+            document.getElementById('inputBox').innerHTML = "<p>Ooops, your DB seems to have multiple users... FAIL</p>"
         }
         else if(msg == 1) {
-            dbOK = true;
-            usernameSetAtStartup = true;
+            addUser();
         }
         else if(msg < 1){
-            dbOK = true;
+            document.getElementById('inputBox').innerHTML = inputUsername();
+            document.getElementById('buttonUsername').onclick = function() {
+                ipcRenderer.send('emitSetUsername', document.getElementById('inputUsername').value);
+            };
+
+            ipcRenderer.on("responseSetUsername", function (event, msg) {
+                console.log("Set Username : " + msg);
+                if(msg.toString() == "OK"){
+                    addUser();
+                }
+            });
         }
     });
-
-    if(usernameSetAtStartup && dbOK){
-        addUser();
-    }
-    else if(dbOK){
-        document.getElementById('inputBox').innerHTML = inputUsername();
-        document.getElementById('buttonUsername').onclick = function() {
-            ipcRenderer.send('emitSetUsername', document.getElementById('inputUsername').value);
-        };
-
-        ipcRenderer.on("responseSetUsername", function (event, msg) {
-            console.log("Set Username : " + msg);
-            if(msg.toString() == "OK"){
-                addUser();
-            }
-        });
-    }
-    else {
-        document.getElementById('inputBox').innerHTML = "<p>Ooops, your DB seems to have multiple users... FAIL</p>"
-    }
 
 });
